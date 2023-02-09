794d1eb53df5468247825f4f1336cd203ce52f67
Aashna Kapoor <67586621+aashnak990@users.noreply.github.com>
2023-02-09 11:36:34 +0530

    Delete frontend directory

--- frontend/src/pages/Logout.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from "react-router-dom";
-
-export function loader() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expiration');
-    console.log("hi");
-    return redirect('/');
-}
